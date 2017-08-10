5d6179db4f2b145dde989a0042d2826b02e7eea5
Aileen Novero <novero.a@gmail.com>
2017-08-10 13:28:31 -0400

    Adding new models and database tables; showing utility of django admin and foreign key relationships.

--- bucketlist/rest_api/admin.py
+++ bucketlist/rest_api/admin.py
@@ -1,6 +1,67 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import Profile, Bucketlist, BucketListCategory, BucketListDetail
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'is_staff',
+                    'get_role',
+                    'get_location')
+
+    list_select_related = ('profile',)
+
+    def get_location(self, instance):
+        return instance.profile.location
+
+    def get_role(self, instance):
+        return instance.profile.get_role_display()
+
+    get_location.short_description = 'Location'
+    get_role.short_description = 'Affiliation'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+class BucketListDetailInline(admin.StackedInline):
+    model = BucketListDetail
+
+
+@admin.register(Bucketlist)
+class BucketlistAdmin(admin.ModelAdmin):
+    inlines = [
+        BucketListDetailInline
+    ]
+
+
+@admin.register(BucketListCategory)
+class BucketCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+    # TODO https://simpleisbetterthancomplex.com/tutorial/2017/03/14/how-to-create-django-admin-list-actions.html
--- bucketlist/rest_api/models.py
+++ bucketlist/rest_api/models.py
@@ -1,8 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # /rest_api/models.py
+
+
+class Profile(models.Model):
+    VISITOR = 1
+    COLLEAGUE = 2
+    SUPERUSER = 3
+    role_choices = (
+        (VISITOR, 'Visitor'),
+        (COLLEAGUE, 'Colleague'),
+        (SUPERUSER, 'Superuser'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=role_choices, null=True, blank=True)
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+
+class BucketListCategory(models.Model):
+    category = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}".format(self.category)
+
+    class Meta:
+        verbose_name = 'BucketListCategory'
+        verbose_name_plural = 'BucketListCategories'
 
 class Bucketlist(models.Model):
     """
@@ -14,7 +57,28 @@ class Bucketlist(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    list_category = models.ForeignKey(BucketListCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
+
+
+class BucketListDetail(models.Model):
+    name = models.OneToOneField(Bucketlist, on_delete=models.CASCADE)
+    dreamer = models.CharField(max_length=30, blank=True, null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    reason = models.TextField(blank=True, null=True)
+    status = models.NullBooleanField(blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    class Meta:
+        verbose_name = 'BucketListDetail'
+        verbose_name_plural = 'BucketListDetails'
